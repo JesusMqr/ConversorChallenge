d296aab7488ae5e63aed8db7db19de4a4fd1e6a5
Jesus <jesusmaquera10@gmail.com>
2023-07-17 17:12:35 -0500

    Interfaz grafica del programa V1

---
+++ src/main/java/challenge/challengeconversor/ChallengeConversor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package challenge.challengeconversor;
+
+import GUI.Window;
+
+
+/**
+ *
+ * @author Jesus
+ */
+public class ChallengeConversor {
+
+    public static void main(String[] args) {
+        Window inicio = new Window();
+        inicio.setVisible(true);
+
+        
+    }
+}
